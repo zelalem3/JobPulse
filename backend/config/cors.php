0a0f02218d6b796ffbe3bee8b511c5c0e3a2a871
zelalem3 <zgetnet24@gmail.com>
2026-06-25 09:27:23 +0300

    fix: fixed cors issue and also removed version from docker-compose to resolve confusion

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173',
+        'http://127.0.0.1:5173')],
 
     'allowed_origins_patterns' => [],
 
