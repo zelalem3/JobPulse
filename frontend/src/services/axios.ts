a4aa945bb82fee8c910aed0c5be98e22b3f31bec
zelalem3 <zgetnet24@gmail.com>
2026-06-25 09:26:33 +0300

    fix: fixed cors issue

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://localhost:8080/api",
   headers: {
     Accept: "application/json",
   },
