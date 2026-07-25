8f40244ebdcd5b61e2e271f2d8252b063b55f296
zelalem3 <zgetnet24@gmail.com>
2026-07-25 22:32:32 +0300

    fix(scraper): fixed username for Josad telegram scaper

--- scrapers/run.py
+++ scrapers/run.py
@@ -22,7 +22,7 @@ SCRAPERS = [
     EthioJob(),
  
     TelegramChannelScraper("effoyjobs"),
-    JosadTelegramScraper("Josad"),
+    JosadTelegramScraper("josad_software"),
     EtcarrerTelegramScraper("etcareersjobs"),
     JobsEthioTelegramScraper("jobs_in_ethio")
 
