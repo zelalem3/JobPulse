0ff3ee08faaf07b6d2533cd3259652f76cacc52e
zelalem3 <zgetnet24@gmail.com>
2026-09-14 11:21:40 +0300

    fix(frontend): fixed axios configuration

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8080',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://jobpulse-api-5zhn.onrender.com',
   withCredentials: true,
   withXSRFToken: true,           
   headers: {
